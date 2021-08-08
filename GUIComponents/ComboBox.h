b0119baecd7dfe0d6c89404c0ae71bcbd0c63bf5
UnknownException <UnknownException@users.noreply.github.com>
2021-08-08 14:21:32 +0200

    Simplify ComboBox

--- GUIComponents/ComboBox.h
+++ GUIComponents/ComboBox.h
@@ -5,73 +5,69 @@
 #endif
 
 class ComboBox : public Item {
-	std::vector<LPCWSTR> preStrings;
-	int preSelection;
+	std::vector<LPCWSTR> _content; // Contains a copy of the combobox items, at the cost of a little bit more memory usage
+	int _selection;
 
-	void ClearPreSelection() {
-		for(auto it = preStrings.begin(); it != preStrings.end(); ++it)
-			delete[] *(it);
+	void ClearContent() {
+		for(auto it = _content.begin(); it != _content.end(); ++it)
+			delete[] *it;
 
-		preStrings.clear();		
+		_content.clear();		
 	}
 
-public:
-	void AddString(LPCWSTR string) {
-		if (GetSelf())
-		{
-			if (GetCount() == 0)
+	void AddStringToWin32Component(LPCWSTR string) {
+		if (GetSelf()) {
+			if (static_cast<int>(SendMessage(GetSelf(), CB_GETCOUNT, 0, 0)) == 0)
 				SetWindowText(GetSelf(), string);
 
 			SendMessage(GetSelf(), CB_ADDSTRING, 0, (LPARAM)string);
-		}
-		else
-		{
-			WCHAR* output = new WCHAR[wcslen(string) + 1];
-			wcscpy_s(output, wcslen(string) + 1, string);
-			preStrings.push_back(output);
-		}
+		}		
 	}
-
-	int GetSelection() {
-		if (GetSelf())
-			return static_cast<int>(SendMessage(GetSelf(), CB_GETCURSEL, 0, 0));
-
-		return 0;
-	}
-	void SetSelection(int index) {
-		if (GetSelf())
-			SendMessage(GetSelf(), CB_SETCURSEL, (WPARAM)index, 0);
-		else
-			preSelection = index;
-	}
-
-	int GetCount() {
-		if (GetSelf())
-			return static_cast<int>(SendMessage(GetSelf(), CB_GETCOUNT, 0, 0));
+public:
+	LPCWSTR GetString(int index) {
+		if(index < 0 || index >= _content.size())
+			return nullptr;
 		
-		return static_cast<int>(preStrings.size());	
+		return _content[index];
+	}
+	void AddString(LPCWSTR string) {
+		WCHAR* output = new WCHAR[wcslen(string) + 1];
+		wcscpy_s(output, wcslen(string) + 1, string);
+			
+		AddStringToWin32Component(output);
+
+		_content.push_back(output);
 	}
 
+	int GetSelection() { return _selection; }
+	void SetSelection(int index) {
+		_selection = index;
+		
+		if (GetSelf())
+			SendMessage(GetSelf(), CB_SETCURSEL, (WPARAM)_selection, 0);
+	}
+
+	int GetCount() { return static_cast<int>(_content.size()); }
+		
 	ComboBox() {
 		SetClassname(L"COMBOBOX");
 		SetStyle(WS_VISIBLE | WS_CHILD | WS_OVERLAPPED | WS_VSCROLL | CBS_DROPDOWN | CBS_HASSTRINGS);
 
-		preSelection = -1;
+		_selection = 0;
 	}
 	virtual ~ComboBox() {
-		ClearPreSelection();
+		ClearContent();
 	}
 	
 private:
 	virtual bool BeforeCreate() override { return true; }
 	bool AfterCreate() override {
-		for (auto it = preStrings.begin(); it != preStrings.end(); ++it)
-			AddString(*(it));
+		for (auto it = _content.begin(); it != _content.end(); ++it)
+			AddStringToWin32Component(*it);
 
-		if (preSelection >= 0 && preSelection < static_cast<int>(preStrings.size()))
-			SetSelection(preSelection);
+		if (_selection >= 0 && _selection < GetCount())
+			SetSelection(_selection);
 
-		ClearPreSelection();
 		return true;
 	}	
 };
